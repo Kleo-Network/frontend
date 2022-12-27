7e1605099d249834b9fcb1db0fd46699e1f96c3c
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2022-12-28 03:20:53 +0530

    testing CI

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -88,7 +88,7 @@ function HeroHome() {
                   <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
                   <path d="M10 17l6-5-6-5z" />
                 </svg>
-                <span className="ml-3">Watch the full video (2 min)</span>
+                <span className="ml-3">Watch the full video now (2 min)</span>
               </button>
             </div>
 
