cb9bec05db7951c42b8464e6b372b6fa7c02a15f
aish96 <aish.96jain@gmail.com>
2023-08-27 03:02:57 +0530

    added API call for creating org

--- src/partials/DocumentationContent.jsx
+++ src/partials/DocumentationContent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   DocumentationOptions,
   DocumentationOptionsText,
@@ -6,6 +6,7 @@ import {
 } from "../utils/constants";
 import CookieManager from "./CookieManager/CookieManager";
 import ParagraphSection from "./ParagraphSection";
+import axios from "axios";
 
 const DocumentationContent = ({
   step,
@@ -13,10 +14,51 @@ const DocumentationContent = ({
   browsingTiers,
   setBrowsingTiers,
 }) => {
+  let [loading, setLoading] = useState(false);
+  let [intent, setIntent] = useState('');
+  let [error, setError] = useState(false);
+
   const removeTier = (index) => {
     const tempState = [...browsingTiers.filter((val, idx) => idx !== index)];
     setBrowsingTiers(tempState);
   };
+
+  const formatTierData = () => {
+    return browsingTiers.map(({tierName, lookups}) => {
+      return {
+        header: tierName,
+        default: true,
+        subMenu: lookups.map(({lookup, indexedTimes, lastDays})=> {
+          return {
+            index: lookup,
+            times: indexedTimes,
+            days: lastDays,
+            default: true,
+            state: "selected"
+          }
+        })
+      }
+    })
+  }
+
+  const uploadOrgData = async () => {
+    try {
+      setLoading(true);
+      setError(false);
+      let data = await  axios.post(`http://0.0.0.0:8000/api/organizations/`, {
+        intent: intent,
+        intentGeneric: "",
+        domainUrl: "",
+        formData: formatTierData()
+      })
+        setStep(DocumentationOptions.EMBED);
+    } catch (error) {
+      console.log("error in creating org:", error);
+      setError(true)
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <div className="md:grow">
@@ -259,6 +301,8 @@ const DocumentationContent = ({
                   labelHidden
                   hasIcon="right"
                   placeholder="Intent"
+                  value={intent}
+                  onChange={(e)=>setIntent(e.currentTarget.value)}
                 ></textarea>
                 <div
                   style={{ textAlign: "right" }}
@@ -271,11 +315,24 @@ const DocumentationContent = ({
                     </button>
                   </div>
                 </div>
+                {error && <div class="text-sm bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-3 rounded relative" role="alert">
+                  <span class="block sm:inline">Something went wrong, please try again.</span>
+                  <span class="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={()=> setError(false)}>
+                    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                  </span>
+                </div>}
                 <button
-                  onClick={() => setStep(DocumentationOptions.EMBED)}
-                  className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
+                  disabled={loading}
+                  onClick={() => uploadOrgData()}
+                  className={`btn text-white bg-blue-600 hover:bg-blue-700 w-full ${loading ? "cursor-not-allowed opacity-25" : ""}`}
                 >
                   Next &#62;&#62; {DocumentationOptionsText.EMBED}
+                  {loading && (<>
+                    <svg aria-hidden="true" class="inline w-8 h-8 ml-3 fill-gray-200 animate-spin text-blue-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                  </svg>
+                  </>)}
                 </button>
               </div>
             </div>
