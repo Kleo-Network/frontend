305414497ac031cce0bc5594fa2d8420415ec30d
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-09-13 13:13:25 +0530

    fix: pinned websites

--- src/components/profile-v2/Profile.tsx
+++ src/components/profile-v2/Profile.tsx
@@ -221,7 +221,7 @@ const Profile: React.FC = () => {
             className="font-bold text-lg p-5 text-gray-600 bg-white shadow-md rounded-3xl"
             contentEditable
           >
-            Top Pinned Searches
+            Pinned Websites
           </div>
           <StarredCards topPinned={user.topPinned} />
         </div>
