176c006570d14cc169a3beb6c7c414cbb4b48209
aish96 <aish.96jain@gmail.com>
2023-09-26 21:27:13 +0530

    Added History Tab content to pinned web drawer

--- src/components/charts/AreaChart.tsx
+++ src/components/charts/AreaChart.tsx
@@ -7,143 +7,89 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
-  Legend,
-  Bar
+  Bar,
+  Label
 } from 'recharts'
-import MultiProgressBar from './MultiProgressBar'
 
-interface BrowsingData {
+export interface BrowsingData {
   name: string
   amt: number
 }
 
-interface Website {
-  label: string
-  link: string
-  percentage: number
-  colorClass: string
-}
-interface CustomTooltipProps {
-  websites: Website[]
-  // add other props here
-}
-
 interface BrowsingHistoryChartProps {
   browsingData: BrowsingData[]
-  websitesArr: Website[]
-}
-
-const CustomTooltip: React.FC<CustomTooltipProps> = ({ websites }) => {
-  //   if (active && payload && payload.length) {
-  return (
-    <div className="custom-tooltip rounded-3xl bg-white p-3 shadow-lg text-xs">
-      <h3 className="text-center text-lg text-gray-700 font-medium leading-8 mb-3 shadow-sm bg-slate-100 rounded">
-        Time Snapshot
-      </h3>
-      <MultiProgressBar progressBars={websites} />
-      <div className="flex flex-row flex-wrap justify-between">
-        {websites.map((bar, index) => (
-          <div className="" key={index}>
-            <div className=" py-2">
-              <div className="">
-                <div
-                  data-te-chip-init
-                  data-te-ripple-init
-                  className={`[word-wrap: break-word] my-[5px] flex h-[42px] cursor-pointer items-center justify-between rounded-[21px] ${bar.colorClass} px-[12px] py-0 text-[13px] font-normal normal-case leading-loose text-white shadow-none transition-[opacity] duration-300 ease-linear hover:!shadow-none`}
-                >
-                  <img
-                    className="my-0 -ml-[12px] mr-[8px] h-10 w-10 rounded-[100%]"
-                    src={bar.link}
-                    alt="Contact Person"
-                  />
-                  {bar.label}
-                  <span
-                    data-te-chip-close
-                    className="float-right w-4 cursor-pointer pl-[8px] text-[16px] text-white opacity-[.83] transition-all duration-200 ease-in-out"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-3 w-3 fill-white"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-  //   }
-
-  return null
 }
 
 const BrowsingHistoryChart: React.FC<BrowsingHistoryChartProps> = ({
-  browsingData,
-  websitesArr
+  browsingData
 }) => {
   return (
-    <div className="grid grid-cols-3 gap-5">
-      <div className="bg-[#1e293b] p-5 rounded-3xl col-span-2 shadow-lg">
-        <div className="pb-3">
-          <h3 className="text-white text-lg font-bold">Browsing History</h3>
-          <span className="text-gray-400 text-sm">
-            Your visual journey on the internet
-          </span>
-        </div>
-        <ResponsiveContainer width="100%" aspect={3}>
-          <AreaChart
-            width={400}
-            height={400}
-            data={browsingData}
-            margin={{
-              top: 20,
-              right: 0,
-              left: 0,
-              bottom: 0
-            }}
-            onClick={(e) => console.log(e)}
-          >
-            <defs>
-              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#818cf8" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-
-            <CartesianGrid strokeDasharray="1 1" fill="#1e293b" opacity={0.3} />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            {/* <Legend /> */}
-
-            <Area
-              type="monotone"
-              dataKey="amt"
-              stroke="#818cf8"
-              strokeWidth={3}
-              fillOpacity={1}
-              fill="url(#colorPv)"
-            />
-            <Bar dataKey="amt" barSize={20} fill="#fff" />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
-      <div>
-        <CustomTooltip websites={websitesArr} />
-      </div>
-    </div>
+    <ResponsiveContainer width="100%" height="100%">
+      <AreaChart
+        data={browsingData}
+        margin={{
+          top: 30,
+          right: 40,
+          left: 30,
+          bottom: 30
+        }}
+        onClick={(e) => console.log(e)}
+      >
+        <defs>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#7F56D9" stopOpacity={0.4} />
+            <stop offset="95%" stopColor="#fff" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <CartesianGrid
+          stroke="#F2F4F7"
+          strokeDasharray="0 0"
+          vertical={false}
+        />
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          fontSize={12}
+          fill="#667085"
+          tickLine={false}
+        >
+          <Label
+            value="Month"
+            offset={-8}
+            position="insideBottom"
+            fontSize={12}
+            fill="#667085"
+          />
+        </XAxis>
+        <YAxis axisLine={false} fontSize={12} fill="#667085" tickLine={false}>
+          <Label
+            value="Website Visits"
+            position="insideLeft"
+            angle={-90}
+            fontSize={12}
+            fill="#667085"
+          />
+        </YAxis>{' '}
+        <Tooltip
+          cursor={{
+            stroke: '#98A2B3',
+            strokeWidth: 1,
+            strokeDasharray: '5 5',
+            fill: '#F2F4F7'
+          }}
+          content={() => null}
+        />{' '}
+        <Area
+          type="monotone"
+          dataKey="amt"
+          stroke="#7F56D9"
+          strokeWidth={2}
+          fillOpacity={1}
+          fill="url(#colorPv)"
+        />
+        <Bar dataKey="amt" barSize={20} fill="#fff" />
+      </AreaChart>
+    </ResponsiveContainer>
   )
 }
 
---
+++ src/components/profile/SelectedHistoryTabData.tsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import AreaChart from '../charts/AreaChart'
+import { BrowsingData } from '../charts/AreaChart'
+import { ReactComponent as StarIcon } from '../../assets/images/star.svg'
+import { ReactComponent as EyeIcon } from '../../assets/images/eye.svg'
+
+interface SelectedHistoryTabDataProps {
+  areaChartData: BrowsingData[]
+  currentBrowserHistory: CurrentBrowserHistory
+}
+
+export interface CurrentBrowserHistory {
+  [key: string]: WebsiteHistoryProps[]
+}
+
+interface WebsiteHistoryProps {
+  name: string
+  url: string
+  time: string
+  visits: number
+}
+
+export function SelectedHistoryTabData({
+  areaChartData,
+  currentBrowserHistory
+}: SelectedHistoryTabDataProps) {
+  return (
+    <div className="data-[te-tab-active]:block" role="tabpanel">
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row self-stretch items-center justify-between gap-1 pb-4">
+          <span className="text-lg text-gray-800 font-semibold">History</span>
+          <div
+            className="inline-flex  text-sm font-regular text-gray-700 shadow-sm rounded-lg"
+            role="group"
+          >
+            <button
+              type="button"
+              className="inline-block rounded-l-lg border border-gray-200 px-4 py-[10px]"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              24 Hours
+            </button>
+            <button
+              type="button"
+              className="-ml-0.5 inline-block border border-gray-200 px-4 py-[10px]"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              7 days
+            </button>
+            <button
+              type="button"
+              className="-ml-0.5 inline-block border border-gray-200 px-4 py-[10px]"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              30 days
+            </button>
+            <button
+              type="button"
+              className="-ml-0.5 inline-block rounded-r-lg border border-gray-200 px-4 py-[10px]"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              12 months
+            </button>
+          </div>
+        </div>
+        <div className="h-[260px] w-full flex items-center  rounded-lg border border-gray-200">
+          <AreaChart browsingData={areaChartData} />
+        </div>
+        {Object.keys(currentBrowserHistory).map((key, index) => (
+          <div
+            key={index}
+            className="flex flex-col mt-4 self-stretch items-start justify-start border border-gray-200 rounded-lg"
+          >
+            <div className="text-xs self-stretch text-gray-400 font-semibold p-4 border-b border-gray-200 capitalize">
+              {key}
+            </div>
+            <div className="flex flex-col self-stretch mx-6">
+              {currentBrowserHistory[key].map(
+                ({ time, name, visits, url }, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-row gap-4 justify-between items-center self-stretch py-3 border-b border-gray-200 last:border-b-0"
+                  >
+                    <div className="text-xs text-gray-500 font-regular pt-1">
+                      {time}
+                    </div>
+                    <div className="flex flex-col flex-1 items-start justify-center">
+                      <span className="text-sm text-gray-800 font-medium overflow-hidden overflow-ellipsis whitespace-nowrap">
+                        {name}
+                      </span>
+                      <a
+                        href={url}
+                        title={url}
+                        target="_blank"
+                        className="text-xs text-gray-400 font-regular overflow-hidden overflow-ellipsis whitespace-nowrap hover:text-purple-700"
+                      >
+                        {url}
+                      </a>
+                      <span className="text-xs pt-1 text-gray-500 font-medium">
+                        {visits} visits
+                      </span>
+                    </div>
+                    <StarIcon className="w-6 h-6 cursor-pointer stroke-gray-300 hover:stroke-purple-700" />
+                    <EyeIcon className="w-6 h-6 cursor-pointer stroke-gray-300 hover:stroke-purple-700" />
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
