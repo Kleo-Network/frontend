b42bc46af00c7b4040ef46ada62db1641825d7f0
aish96 <aish.96jain@gmail.com>
2023-10-16 04:19:17 +0530

    fix: modal open in profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -11,7 +11,7 @@ import { UserContext } from '../common/contexts/UserContext'
 export default function Profile() {
   const { user } = useContext(UserContext)
 
-  const [isModalOpen, setIsModalOpen] = useState(true)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
     <section>
