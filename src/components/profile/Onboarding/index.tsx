74edd9b54df93da8559bef1bc317bc215751f9f3
aish96 <aish.96jain@gmail.com>
2023-10-27 03:30:23 +0530

    feat: add wallet sign in msg and connect

--- src/components/profile/Onboarding/index.tsx
+++ src/components/profile/Onboarding/index.tsx
@@ -1,65 +1,69 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ReactComponent as Kleo } from '../../../assets/images/kleoWithBg.svg'
 import { ReactComponent as PhantomLogo } from '../../../assets/images/phantom.svg'
 import { ReactComponent as Arrow } from '../../../assets/images/arrow.svg'
 import { ReactComponent as Tick } from '../../../assets/images/check.svg'
 import Accordion from '../../common/Accordion'
+import { usePhantomWallet } from '../../common/hooks/usePhantomWallet'
+import { PublicKey } from '@solana/web3.js'
 
 interface OnboardingProps {
   closeModal: () => void
 }
 
+enum PluginState {
+  CHECKING,
+  NOT_INSTALLED,
+  INSTALLED
+}
+
 export default function Onboarding({ closeModal }: OnboardingProps) {
-  const [infoExpanded, setInfoExpanded] = React.useState(false)
-  const [isPluginInstalled, setIsPluginInstalled] = React.useState(false)
-  const [isLensConnected, setIsLensConnected] = React.useState(false)
-  const [solanaProvider, setSolanaProvider] = React.useState(null)
+  const [infoExpanded, setInfoExpanded] = useState(false)
+  const [pluginState, setPluginState] = useState(PluginState.CHECKING)
+  const {
+    connected: isWalletConnected,
+    connect,
+    signMessage
+  } = usePhantomWallet()
+
+  const [message, setMessage] = useState<string>('Sign in to Kleo')
+  const [signedData, setSignedData] = useState<{
+    signature: Uint8Array
+    publicKey: PublicKey
+  } | null>(null)
+
+  const handleSign = async () => {
+    if (message) {
+      const result = await signMessage(message)
+      setSignedData(result)
+    }
+  }
 
   useEffect(() => {
-    if (!isPluginInstalled) {
+    if (pluginState === PluginState.CHECKING) {
       setTimeout(() => {
-        console.log('checking for plugin', (window as any).kleoConnect)
         if (
           (window as any).kleoConnect &&
           (window as any).kleoConnect.extension
         ) {
-          setIsPluginInstalled(true)
+          setPluginState(PluginState.INSTALLED)
+        } else {
+          setPluginState(PluginState.NOT_INSTALLED)
         }
       }, 2000)
     }
   }, [])
 
   useEffect(() => {
-    if ('solana' in window) {
-      const provider = window.solana
-      if ((provider as any).isPhantom) {
-        console.log('Phantom is installed!')
-        setSolanaProvider(provider as any)
-      }
-    } else {
-      console.log('Phantom not installed.')
-      // You can redirect the user to the Phantom installation page here
-    }
-  }, [])
-
-  const connectWallet = async () => {
-    if (solanaProvider) {
-      try {
-        await (solanaProvider as any).connect()
-        console.log('Wallet connected')
-        console.log('Public key', (solanaProvider as any).publicKey.toString())
-        setIsLensConnected(true)
-      } catch (err) {
-        console.warn(err)
-      }
-    }
-  }
-
-  useEffect(() => {
-    if (isPluginInstalled && isLensConnected) {
+    if (
+      pluginState === PluginState.INSTALLED &&
+      isWalletConnected &&
+      signedData &&
+      signedData.publicKey
+    ) {
       closeModal()
     }
-  }, [isPluginInstalled, isLensConnected])
+  }, [pluginState, isWalletConnected, signedData])
 
   return (
     <div className="flex flex-col items-start">
@@ -68,7 +72,7 @@ export default function Onboarding({ closeModal }: OnboardingProps) {
       </div>
       <div className="flex flex-row items-start gap-4 p-6">
         <div className="relative">
-          {isPluginInstalled && (
+          {pluginState === PluginState.INSTALLED && (
             <div className="absolute bottom-0 left-auto right-0 top-auto z-10 rounded-full bg-green-600 p-1 border-4 border-white ">
               <Tick className="w-3 h-3 fill-white" />
             </div>
@@ -84,11 +88,21 @@ export default function Onboarding({ closeModal }: OnboardingProps) {
             Unlock insights, personalize your Browsing and safeguard your
             privacy
           </span>
-          {!isPluginInstalled && (
+          {pluginState === PluginState.CHECKING && (
+            <div
+              className="inline-block m-1 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          )}
+          {pluginState === PluginState.NOT_INSTALLED && (
             <div className="flex flex-row justify-start items-center mt-4 text-sm font-medium">
               <button
                 className="px-4 py-3 bg-primary text-white rounded-lg shadow mr-1"
-                onClick={() => setIsPluginInstalled(true)}
+                onClick={() => setPluginState(PluginState.INSTALLED)}
               >
                 Install Plugin
               </button>
@@ -101,30 +115,58 @@ export default function Onboarding({ closeModal }: OnboardingProps) {
       </div>
       <div className="flex flex-row items-start gap-4 p-6">
         <div className="relative">
-          {isLensConnected && (
+          {isWalletConnected && (
             <div className="absolute bottom-0 left-auto right-0 top-auto z-10 rounded-full bg-green-600 p-1 border-4 border-white">
               <Tick className="w-3 h-3 fill-white" />
             </div>
           )}
           <PhantomLogo className="w-16 h-16 fill-[#ab9ff2]" />
         </div>
-        <div className="flex flex-col items-start justify-center">
-          <span className="text-gray-900 text-base font-medium">
-            Connect Phantom Wallet
-          </span>
-          <span className="text-gray-400 text-sm font-regular">
-            Unlock insights, personalize your Browsing and safeguard your
-            privacy
-          </span>
-          <div className="flex flex-row justify-start items-center mt-4 text-sm font-medium">
-            <button
-              className="px-4 py-3 bg-primary text-white rounded-lg shadow mr-1"
-              onClick={() => connectWallet()}
-            >
-              Connect
-            </button>
+        {!isWalletConnected ? (
+          <div className="flex flex-col items-start justify-center">
+            <span className="text-gray-900 text-base font-medium">
+              Connect Phantom Wallet
+            </span>
+            <span className="text-gray-400 text-sm font-regular">
+              Connect with your Phantom wallet to get started
+            </span>
+            <div className="flex flex-row justify-start items-center mt-4 text-sm font-medium">
+              <button
+                className="px-4 py-3 bg-primary text-white rounded-lg shadow mr-1"
+                onClick={connect}
+              >
+                Connect
+              </button>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div>
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Enter message to sign"
+              className="w-full p-2 border rounded mb-4"
+            />
+            <button
+              onClick={handleSign}
+              className="px-4 py-2 bg-primary text-white rounded mb-4"
+            >
+              Sign Message
+            </button>
+            {signedData && (
+              <div className="bg-white p-4 rounded shadow-md">
+                {/* <p className="mb-2 font-semibold">Signature:</p> 
+                 <code className="text-sm bg-gray-200 p-2 rounded">
+                  {Array.from(signedData.signature).join(', ')}
+                </code> */}
+                <p className="mt-4 mb-2 font-semibold">Public Key:</p>
+                <code className="text-sm bg-gray-200 p-2 rounded">
+                  {signedData.publicKey.toString()}
+                </code>
+              </div>
+            )}
+          </div>
+        )}
       </div>
       <div className="p-6 border-t border-gray-200 w-full">
         <Accordion
