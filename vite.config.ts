3c12e6b101dffae598b70ca1374eaf2a407d0d72
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-11-12 19:54:55 +0300

    fix: nearHackathon

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// npm install --dev @esbuild-plugins/node-modules-polyfill
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+// You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()]
+  base: '/',
+  plugins: [react(), svgr()],
+  build: {
+    commonjsOptions: {
+      include: []
+    },
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()]
+    }
+  },
+  optimizeDeps: {
+    disabled: false,
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  }
 })
