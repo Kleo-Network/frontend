a59a0585344d0c0a65fcbb2d3baeb15f30d3b5e1
princedalsaniya <prince.d@ahduni.edu.in>
2024-09-08 17:23:01 +0530

    feat: featureGrid for mobile to sm sizes

---
+++ src/pages/home/FeatureGridSection.tsx
@@ -0,0 +1,80 @@
+import { ReactComponent as GridFirstImage } from '../../assets/images/home/grid_first_image.svg'
+import { ReactComponent as GridSecondImage } from '../../assets/images/home/grid_second_image.svg'
+import { ReactComponent as GridThirdImage } from '../../assets/images/home/grid_third_image.svg'
+
+export const FeatureGridSection = () => {
+  return (
+    <section className="w-full bg-gray-900 flex justify-center rounded-[50px] text-white mt-[60px] sm:mt-[80px] lg:mt-[100px] mac:mt-[120px] 2xl:mt-[150px] py-[60px] md:py-[80px] lg:py-[100px] mac:py-[120px] 2xl:py-[150px] px-4 sm:px-[100px] md:px-[140px] lg:px-[160px] xl:px-[150px] 2xl:px-[200px]">
+      <div className="max-w-[440px] sm:max-w-[488px] md:max-w-[664px] lg:max-w-[980px] xl:max-w-[1140px] mac:max-w-[1140px] 2xl:max-w-[1520px] flex w-full h-full items-center">
+        <div className="flex flex-col gap-[60px] items-center">
+          {/* Pair - 1 */}
+          <div className="flex flex-col gap-6 items-center">
+            {/* Image */}
+            <GridFirstImage className="w-full h-auto rounded-lg" />
+            {/* Text */}
+            <div className="flex flex-col gap-1 items-start w-full">
+              <div className="font-semibold text-2xl">
+                Own A Stake For Your Data
+              </div>
+              <p className="font-roboto font-normal text-[11px]">
+                Machine Learning Models / Analytics Houses want this data. Kleo
+                and VANA DAO will come up with a proof of contribution which
+                will then decide on user contribution tokens/$ amount that will
+                be shared based on the requirement. Rest assured no personally
+                identifiable information will be leaked.
+                <br />
+                <br />
+                We will be using snapshot for Kleo token holders to manage
+                conversation between data requirement and data liquidity
+                provider.
+              </p>
+            </div>
+          </div>
+
+          {/* Pair - 2 */}
+          <div className="flex flex-col gap-6 items-center">
+            {/* Image */}
+            <GridSecondImage className="w-full h-auto rounded-lg" />
+            {/* Text */}
+            <div className="flex flex-col gap-1 items-start w-full">
+              <div className="font-semibold text-2xl">Earn Rewards</div>
+              <p className="font-roboto font-normal text-[11px]">
+                Based on intelligence and complexity of your actions inside the
+                browser, kleo will reward kleo XP points. Kleo will be
+                transparent about the activity that helps you get the maximum
+                kleo points but the exact algorithm will be closed source to
+                ensure there is no farming.
+                <br />
+                <br />
+                Just do what you do and earn rewards on the basis of the
+                activity.
+              </p>
+            </div>
+          </div>
+
+          {/* Pair - 3 */}
+          <div className="flex flex-col gap-6 items-center">
+            {/* Image */}
+            <GridThirdImage className="w-full h-auto rounded-lg" />
+            {/* Text */}
+            <div className="flex flex-col gap-1 items-start w-full">
+              <div className="font-semibold text-2xl">Kleo SDK</div>
+              <p className="font-roboto font-normal text-[11px]">
+                Earn from your extension! Be a part of Kleo Data DAO.
+                <br />
+                <br />
+                Personalize your DAPP.
+              </p>
+              <a
+                href="#"
+                className="font-medium text-sm text-white bg-primary-500 hover:bg-primary-600 px-[14px] py-[8px] rounded-[4px] mt-[12px]"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from './HeroSection'
 import DashboardImage from '../../assets/images/home/KleoDashboard.png'
 import React, { useState } from 'react'
 import { HowKleoWorksSection } from './HowKleoWorksSection'
-import { ReactComponent as GridFirstImage } from '../../assets/images/home/grid_first_image.svg'
+import { FeatureGridSection } from './FeatureGridSection'
 
 // Constants for initial and maximum scale
 const INITIAL_SCALE = 1
@@ -27,7 +27,6 @@ export const HomeComponent = () => {
       className="w-full bg-[#F5F5FA] overflow-y-auto overflow-x-hidden z-40"
       onScroll={listenScrollEvent.bind(this)}
     >
-      {/* Hero Section */}
       <HeroSection />
 
       <div className="flex justify-center relative -top-[50px] lg:-top-[150px] xl:-top-[80px] mac:-top-[90px] 2xl:-top-[110px] mb-[60px]">
@@ -44,53 +43,7 @@ export const HomeComponent = () => {
 
       <HowKleoWorksSection />
 
-      <section className="w-full bg-gray-900 flex justify-center rounded-[50px] py-[100px] text-white">
-        <div className="max-w-[1050px] w-full">
-          <div className="grid grid-cols-2 grid-rows-3 gap-[18px]">
-            <div className=" flex flex-col justify-center items-start">
-              <div className="font-montserrat font-semibold text-[38px]">
-                <div className="">Own A Stake For</div>
-                <div className="">Your Data</div>
-              </div>
-              <div className="[text-wrap: balance] font-roboto font-light text-xs">
-                <p>
-                  Machine Learning Models / Analytics Houses want this data.
-                  Kleo and VANA DAO will come up with a proof of contribution
-                  which will then decide on user contribution tokens/$ amount
-                  that will be shared based on the requirement. Rest assured no
-                  personally identifiable information will be leaked.
-                </p>
-                <p>
-                  We will be using snapshot for Kleo token holders to manage
-                  conversation between data requirement and data liquidity
-                  provider.
-                </p>
-              </div>
-            </div>
-            <div className="">
-              <GridFirstImage className="w-full h-auto" />
-            </div>
-
-            <div className="">
-              <img
-                src="path-to-your-image.jpg"
-                alt="Description"
-                className="w-full h-auto"
-              />
-            </div>
-            <div className="">x</div>
-
-            <div className="">x</div>
-            <div className="">
-              <img
-                src="path-to-your-image.jpg"
-                alt="Description"
-                className="w-full h-auto"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+      <FeatureGridSection />
     </div>
   )
 }
