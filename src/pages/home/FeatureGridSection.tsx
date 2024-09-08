3fe080669a1c86ff1af6c83da985fabe900388c3
princedalsaniya <prince.d@ahduni.edu.in>
2024-09-08 18:44:57 +0530

    feat: featureGridSection all sizes done

--- src/pages/home/FeatureGridSection.tsx
+++ src/pages/home/FeatureGridSection.tsx
@@ -6,17 +6,17 @@ export const FeatureGridSection = () => {
   return (
     <section className="w-full bg-gray-900 flex justify-center rounded-[50px] text-white mt-[60px] sm:mt-[80px] lg:mt-[100px] mac:mt-[120px] 2xl:mt-[150px] py-[60px] md:py-[80px] lg:py-[100px] mac:py-[120px] 2xl:py-[150px] px-4 sm:px-[100px] md:px-[140px] lg:px-[160px] xl:px-[150px] 2xl:px-[200px]">
       <div className="max-w-[440px] sm:max-w-[488px] md:max-w-[664px] lg:max-w-[980px] xl:max-w-[1140px] mac:max-w-[1140px] 2xl:max-w-[1520px] flex w-full h-full items-center">
-        <div className="flex flex-col gap-[60px] sm:gap-[70px] md:gap-[80px] items-center">
+        <div className="flex flex-col gap-[60px] sm:gap-[70px] md:gap-[80px] lg:gap-[100px] items-center lg:w-full">
           {/* Pair - 1 */}
-          <div className="flex flex-col gap-6 items-center">
+          <div className="flex flex-col lg:flex-row-reverse gap-6 lg:gap-[100px] items-center">
             {/* Image */}
-            <GridFirstImage className="w-full h-auto rounded-lg" />
+            <GridFirstImage className="w-full lg:w-1/2 h-auto rounded-lg lg:min-h-[456px] mac:min-h-[560px] 2xl:min-h-[746px]" />
             {/* Text */}
-            <div className="flex flex-col gap-1 md:gap-4 items-start w-full">
-              <div className="font-semibold text-2xl md:text-[32px]">
+            <div className="flex flex-col gap-1 md:gap-4 items-start w-full lg:w-1/2 xl:max-w-[395px]">
+              <div className="font-semibold text-2xl md:text-[32px] xl:text-[42px] xl:leading-tight mac:text-[48px] 2xl:text-[58px]">
                 Own A Stake For Your Data
               </div>
-              <p className="font-roboto font-normal text-[11px] md:text-sm">
+              <p className="font-roboto font-normal text-[11px] md:text-sm mac:text-base 2xl:text-lg">
                 Machine Learning Models / Analytics Houses want this data. Kleo
                 and VANA DAO will come up with a proof of contribution which
                 will then decide on user contribution tokens/$ amount that will
@@ -32,15 +32,15 @@ export const FeatureGridSection = () => {
           </div>
 
           {/* Pair - 2 */}
-          <div className="flex flex-col gap-6 items-center">
+          <div className="flex flex-col lg:flex-row gap-6 lg:gap-[100px] items-center">
             {/* Image */}
-            <GridSecondImage className="w-full h-auto rounded-lg" />
+            <GridSecondImage className="w-full lg:w-1/2 h-auto rounded-lg lg:min-h-[456px] mac:min-h-[560px] 2xl:min-h-[746px]" />
             {/* Text */}
-            <div className="flex flex-col gap-1 md:gap-4 items-start w-full">
-              <div className="font-semibold text-2xl md:text-[32px]">
+            <div className="flex flex-col gap-1 md:gap-4 items-start w-full lg:w-1/2 xl:max-w-[395px]">
+              <div className="font-semibold text-2xl md:text-[32px] xl:text-[42px] xl:leading-tight mac:text-[48px] 2xl:text-[58px]">
                 Earn Rewards
               </div>
-              <p className="font-roboto font-normal text-[11px] md:text-sm">
+              <p className="font-roboto font-normal text-[11px] md:text-sm mac:text-base 2xl:text-lg">
                 Based on intelligence and complexity of your actions inside the
                 browser, kleo will reward kleo XP points. Kleo will be
                 transparent about the activity that helps you get the maximum
@@ -55,16 +55,16 @@ export const FeatureGridSection = () => {
           </div>
 
           {/* Pair - 3 */}
-          <div className="flex flex-col gap-6 items-center">
+          <div className="flex flex-col lg:flex-row-reverse gap-6 lg:gap-[100px] items-center">
             {/* Image */}
-            <GridThirdImage className="w-full h-auto rounded-lg" />
+            <GridThirdImage className="w-full lg:w-1/2 h-auto rounded-lg lg:min-h-[456px] mac:min-h-[560px] 2xl:min-h-[746px]" />
             {/* Text */}
-            <div className="flex flex-col gap-4 items-start w-full">
+            <div className="flex flex-col gap-4 items-start w-full lg:w-1/2 xl:max-w-[395px]">
               <div className="flex flex-col gap-1 md:gap-4 items-start w-full">
-                <div className="font-semibold text-2xl md:text-[32px]">
+                <div className="font-semibold text-2xl md:text-[32px] xl:text-[42px] xl:leading-tight mac:text-[48px] 2xl:text-[58px]">
                   Kleo SDK
                 </div>
-                <p className="font-roboto font-normal text-[11px] md:text-sm">
+                <p className="font-roboto font-normal text-[11px] md:text-sm mac:text-base 2xl:text-lg">
                   Earn from your extension! Be a part of Kleo Data DAO.
                   <br />
                   <br />
