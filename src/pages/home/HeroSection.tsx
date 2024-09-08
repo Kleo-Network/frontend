eb9a65b89b6ec21157dc6e4d3c23ff949c8da272
princedalsaniya <prince.d@ahduni.edu.in>
2024-09-08 11:59:44 +0530

    feat: heroSection from 640 to 768 sizes

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -4,28 +4,28 @@ import { ReactComponent as TopRightGrid } from '../../assets/images/home/hero_to
 
 export const HeroSection = () => {
   return (
-    <section className="h-[520px] pt-[124px] bg-gray-900 flex justify-center relative rounded-bl-[50px] rounded-br-[50px] overflow-hidden">
+    <section className="h-[520px] sm:h-[460px] pt-[124px] sm:pt-[80px] bg-gray-900 flex justify-center relative rounded-bl-[50px] rounded-br-[50px] overflow-hidden">
       <div className="z-[45] h-max max-w-[440px] sm:max-w-[488px] md:max-w-[664px] lg:max-w-[980px] xl:max-w-[1140px] mac:max-w-[1140px] 2xl:max-w-[1520px] flex w-full items-start">
         {/* Hero Text */}
         <div className="flex h-full w-full flex-col justify-start items-start px-4">
           {/* Earn Your + Stack in the AI */}
-          <div className="flex flex-col text-white font-medium text-[40px]">
+          <div className="flex flex-col text-white font-medium text-[40px] sm:text-[50px]">
             <span className="flex items-center justify-start">
               Earn Your
-              <Sparkle className="w-8 h-auto" />
+              <Sparkle className="w-8 sm:w-9 sm:ml-9 h-auto" />
             </span>
-            <span className="ml-10">Stake In The AI</span>
+            <span className="ml-10 sm:ml-20">Stake In The AI</span>
           </div>
           {/* Revolution + Create Account */}
-          <div className="flex flex-col gap-4">
-            <span className="text-white font-medium text-[40px]">
+          <div className="flex flex-col sm:flex-row gap-4 sm:ml-7">
+            <span className="text-white font-medium text-[40px] sm:text-[50px]">
               Revolution
             </span>
-            <div className="flex flex-col font-roboto font-normal text-sm text-white max-w-[240px] gap-4">
+            <div className="flex flex-col font-roboto font-normal text-sm sm:text-[11px] text-white max-w-[240px] sm:max-w-[136px] gap-4 sm:mt-6">
               Earn, Govern and Own your data. Earn upto $400/mo with your data.
               <a
                 href="#"
-                className="py-2 px-7 bg-primary-500 hover:bg-primary-700 text-white font-medium text-[20px] w-max rounded-[4px] font-inter text-sm"
+                className="py-2 px-7 sm:px-[14px] sm:py-2 bg-primary-500 hover:bg-primary-700 text-white font-medium text-[20px] sm:text-sm w-max rounded-[4px] font-inter text-sm"
               >
                 Create Account
               </a>
@@ -33,8 +33,8 @@ export const HeroSection = () => {
           </div>
         </div>
       </div>
-      <BottomLeftGrid className="z-20 absolute bottom-0 left-0 w-[318px] h-auto -translate-x-24 translate-y-24 pointer-events-none" />
-      <TopRightGrid className="z-20 absolute top-0 right-0 w-[318px] h-[292px] pointer-events-none" />
+      <BottomLeftGrid className="z-20 absolute bottom-0 left-0 w-[318px] h-[318px] sm:w-[360px] sm:h-[360px] -translate-x-24 translate-y-24 sm:-translate-x-8 pointer-events-none" />
+      <TopRightGrid className="z-20 absolute top-0 right-0 w-[318px] h-[292px] sm:w-[360px] sm:h-[332px] pointer-events-none" />
     </section>
   )
 }
