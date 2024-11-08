10d0982b8be7ba68cda4d544c6cafdd20c58ff5a
princedalsaniya <prince.d@ahduni.edu.in>
2024-11-09 00:22:11 +0700

    feat: carousel and stats cards are changing layout responsively

--- src/pages/memo/StatsCards.tsx
+++ src/pages/memo/StatsCards.tsx
@@ -3,32 +3,37 @@ import PurpleGridBg from '../../assets/images/PurpleGridBg.svg'
 
 export const StatsCards = () => {
   return (
-    <div className="flex flex-col justify-between gap-5 h-full w-full">
-      {/* Downloads */}
-      <div className="w-full h-full rounded-xl p-5 flex justify-start items-center bg-white gap-5">
-        <div className="w-20 h-20 bg-grayblue-100 rounded-lg"></div>
-        <div className="flex flex-col justify-center items-start">
-          <h1 className="text-4xl font-bold">3450</h1>
-          <h3 className="text-lg font-normal">Downloads</h3>
+    <div className="flex flex-col gap-5 h-full w-full">
+      {/* Wrapper for Downloads and Active Users */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 w-full xl:flex xl:flex-col xl:h-2/3">
+        {/* Downloads */}
+        <div className="w-full h-full rounded-xl p-5 flex justify-start items-center bg-white gap-5">
+          <div className="w-20 h-20 bg-grayblue-100 rounded-lg"></div>
+          <div className="flex flex-col justify-center items-start">
+            <h1 className="text-4xl font-bold">3450</h1>
+            <h3 className="text-lg font-normal">Downloads</h3>
+          </div>
+        </div>
+
+        {/* Active Users */}
+        <div
+          className="w-full h-full rounded-xl p-5 flex justify-start items-center gap-5 text-white"
+          style={{
+            backgroundImage: `url(${PurpleGridBg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover'
+          }}
+        >
+          <div className="w-20 h-20 bg-grayblue-100 rounded-lg"></div>
+          <div className="flex flex-col justify-center items-start">
+            <h1 className="text-4xl font-bold">100000</h1>
+            <h3 className="text-lg font-normal">Active Users</h3>
+          </div>
         </div>
       </div>
-      {/* Active Users */}
-      <div
-        className="w-full h-full rounded-xl p-5 flex justify-start items-center gap-5 text-white"
-        style={{
-          backgroundImage: `url(${PurpleGridBg})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover'
-        }}
-      >
-        <div className="w-20 h-20 bg-grayblue-100 rounded-lg"></div>
-        <div className="flex flex-col justify-center items-start">
-          <h1 className="text-4xl font-bold">100000</h1>
-          <h3 className="text-lg font-normal">Active Users</h3>
-        </div>
-      </div>
-      {/* Onchain Transactions */}
-      <div className="w-full h-full rounded-xl p-5 flex justify-start items-center bg-white gap-5">
+
+      {/* On Chain Transactions */}
+      <div className="w-full h-full rounded-xl p-5 flex justify-start items-center bg-white gap-5 md:col-span-2 xl:h-1/3">
         <div className="w-20 h-20 bg-grayblue-100 rounded-lg"></div>
         <div className="flex flex-col justify-center items-start">
           <h1 className="text-4xl font-bold">55000000</h1>
--- src/pages/memo/index.tsx
+++ src/pages/memo/index.tsx
@@ -8,11 +8,11 @@ const Memo: React.FC = () => {
     <div className="bg-grayblue-100 overflow-y-auto z-40 w-full h-full pt-[100px] flex justify-center items-start">
       <div className="max-w-[440px] sm:max-w-[488px] md:max-w-[664px] lg:max-w-[980px] xl:max-w-[1140px] mac:max-w-[1140px] 2xl:max-w-[1520px] w-full p-6">
         {/* Carousel and Stat Cards */}
-        <div className="grid grid-cols-[0.66fr_0.333fr] gap-5">
-          <div>
+        <div className="grid gap-5 xl:grid-cols-[0.66fr_0.333fr] grid-cols-1">
+          <div className="w-full">
             <Carousel />
           </div>
-          <div>
+          <div className="w-full">
             <StatsCards />
           </div>
         </div>
