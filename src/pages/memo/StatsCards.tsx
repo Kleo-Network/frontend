cd1a60a39e4349a3c37a3aed04cdeeed1835703e
princedalsaniya <prince.d@ahduni.edu.in>
2024-11-09 00:42:32 +0700

    feat: added icons in stats cards

--- src/pages/memo/StatsCards.tsx
+++ src/pages/memo/StatsCards.tsx
@@ -1,5 +1,6 @@
 import rewardImage from '../../assets/images/reward.png'
 import PurpleGridBg from '../../assets/images/PurpleGridBg.svg'
+import { ArrowDownToLine, ArrowRightLeft, Users } from 'lucide-react'
 
 export const StatsCards = () => {
   return (
@@ -7,8 +8,10 @@ export const StatsCards = () => {
       {/* Wrapper for Downloads and Active Users */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 w-full xl:flex xl:flex-col xl:h-2/3">
         {/* Downloads */}
-        <div className="w-full h-full rounded-xl p-5 flex justify-start items-center bg-white gap-5">
-          <div className="w-20 h-20 bg-grayblue-100 rounded-lg"></div>
+        <div className="w-full h-full rounded-xl p-5 flex justify-start items-center bg-white gap-5 text-grayblue-600">
+          <div className="w-20 h-20 bg-grayblue-100 rounded-lg flex items-center justify-center">
+            <ArrowDownToLine className="w-16 h-16" />
+          </div>
           <div className="flex flex-col justify-center items-start">
             <h1 className="text-4xl font-bold">3450</h1>
             <h3 className="text-lg font-normal">Downloads</h3>
@@ -24,7 +27,9 @@ export const StatsCards = () => {
             backgroundSize: 'cover'
           }}
         >
-          <div className="w-20 h-20 bg-grayblue-100 rounded-lg"></div>
+          <div className="w-20 h-20 bg-grayblue-100 rounded-lg flex items-center justify-center">
+            <Users className="w-16 h-16 text-grayblue-600" />
+          </div>
           <div className="flex flex-col justify-center items-start">
             <h1 className="text-4xl font-bold">100000</h1>
             <h3 className="text-lg font-normal">Active Users</h3>
@@ -33,8 +38,10 @@ export const StatsCards = () => {
       </div>
 
       {/* On Chain Transactions */}
-      <div className="w-full h-full rounded-xl p-5 flex justify-start items-center bg-white gap-5 md:col-span-2 xl:h-1/3">
-        <div className="w-20 h-20 bg-grayblue-100 rounded-lg"></div>
+      <div className="w-full h-full rounded-xl p-5 flex justify-start items-center bg-white gap-5 md:col-span-2 xl:h-1/3 text-grayblue-600">
+        <div className="w-20 h-20 bg-grayblue-100 rounded-lg flex items-center justify-center">
+          <ArrowRightLeft className="w-16 h-16" />
+        </div>
         <div className="flex flex-col justify-center items-start">
           <h1 className="text-4xl font-bold">55000000</h1>
           <h3 className="text-lg font-normal">On Chain Transactions</h3>
