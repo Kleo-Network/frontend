89a93fe07d8c898f4732b0381bd0a7c704305b65
princedalsaniya <prince.d@ahduni.edu.in>
2024-11-23 17:02:04 +0530

    feat: added bottom border in hero section

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,7 +4,7 @@ import { AboutHeroSection } from './sections/AboutHeroSection'
 export function About() {
   return (
     <main className="h-full bg-grayblue-200 w-screen">
-      <div className="w-screen h-fit bg-[#0B0E16] flex justify-center relative">
+      <div className="w-screen h-fit bg-[#0B0E16] flex justify-center relative rounded-bl-[30px] rounded-br-[30px] xl:rounded-bl-[50px] xl:rounded-br-[50px]">
         <div className="max-w-[440px] sm:max-w-[488px] md:max-w-[664px] lg:max-w-[980px] xl:max-w-[1140px] mac:max-w-[1140px] 2xl:max-w-[1520px] w-full pt-[44px] lg:pt-[62px] xl:pt-[62px] mac:pt-[66px] 2xl:pt-[88px]">
           {/* Section just bellow Navbar with same width as Navbar in all screen sizes. */}
           <AboutHeroSection />
